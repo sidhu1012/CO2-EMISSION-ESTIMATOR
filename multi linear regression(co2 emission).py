8e8ae8e68e228b1625e5f177e4ed487e15e7f365
sidhu1012 <45732670+sidhu1012@users.noreply.github.com>
2020-07-29 23:38:33 +0530

    Update multi linear regression(co2 emission).py

--- multi linear regression(co2 emission).py
+++ multi linear regression(co2 emission).py
@@ -1,42 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[4]:
-
-
 df=pd.read_csv("https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv")
-
-
-# In[5]:
-
-
 df.head()
-
-
-# In[7]:
-
 
 cdf=df[['ENGINESIZE' , 'CYLINDERS' , 'FUELCONSUMPTION_COMB' , 'CO2EMISSIONS' ]]
 cdf.head(9)
 
 
-# In[8]:
-
-
 cdf.hist()
 plt.show()
 
-
-# In[12]:
 
 
 plt.scatter(cdf.ENGINESIZE ,cdf.CO2EMISSIONS, color='blue')
@@ -53,15 +29,10 @@ plt.ylabel("EMISSIONS")
 plt.show()
 
 
-# In[13]:
-
 
 msk=np.random.rand(len(df))<0.8
 train=cdf[msk]
 test=cdf[~msk]
-
-
-# In[15]:
 
 
 plt.scatter(train.ENGINESIZE , train.CO2EMISSIONS)
@@ -69,8 +40,6 @@ plt.xlabel("ENGINESIZE")
 plt.ylabel("CO2EMISSIONS")
 plt.show()
 
-
-# In[18]:
 
 
 from sklearn import linear_model
@@ -82,9 +51,6 @@ print("Coefficients: ", regr.coef_)
 print("Intercept: ", regr.intercept_)
 
 
-# In[20]:
-
-
 y_hat=regr.predict(test[['ENGINESIZE','FUELCONSUMPTION_COMB']])
 x=np.asanyarray(test[['ENGINESIZE','FUELCONSUMPTION_COMB']])
 y=np.asanyarray(test[['CO2EMISSIONS']])
@@ -92,10 +58,4 @@ print("Residual sum of squares %.2f"
      % np.mean(y_hat-y)**2)
 print("Variance score %.2f"
      % regr.score(x,y))
-
-
-# In[ ]:
-
-
-
 
